eaa40f6ff0505d8234dccb4a2c49f8d6348ce606
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-12-14 22:05:35 -0700

    chore: test vite-tsconfig-paths old version for windows

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,32 +6,32 @@ describe("index", () => {
     });
 
     it("works", async () => {
-        const clippy = await vi.importActual<typeof import("@/clippy")>("@/clippy");
+        const clippy = await vi.importActual<typeof import("@/clippy.ts")>("@/clippy.ts");
 
         using runSpy = vi.spyOn(clippy, "run").mockResolvedValue();
 
-        await vi.importActual("@/index");
+        await vi.importActual("@/index.ts");
 
         expect(runSpy).toHaveBeenCalledTimes(1);
     });
 
     it("catches Error", async () => {
         const core = await vi.importActual<typeof import("@actions/core")>("@actions/core");
-        const clippy = await vi.importActual<typeof import("@/clippy")>("@/clippy");
+        const clippy = await vi.importActual<typeof import("@/clippy.ts")>("@/clippy.ts");
 
         vi.spyOn(clippy, "run").mockRejectedValue(new Error("It looks like you're running a test"));
 
         // eslint-disable-next-line @typescript-eslint/no-empty-function -- mock
         using setFailedSpy = vi.spyOn(core, "setFailed").mockImplementation((_s: Error | string) => {});
 
-        await vi.importActual("@/index");
+        await vi.importActual("@/index.ts");
 
         expect(setFailedSpy).toHaveBeenCalledWith("It looks like you're running a test");
     });
 
     it("catches not-error", async () => {
         const core = await vi.importActual<typeof import("@actions/core")>("@actions/core");
-        const clippy = await vi.importActual<typeof import("@/clippy")>("@/clippy");
+        const clippy = await vi.importActual<typeof import("@/clippy.ts")>("@/clippy.ts");
 
         vi.spyOn(clippy, "run").mockRejectedValue(
             "It looks like you're trying to write a test, would you like some assistance? [YES / NO]",
@@ -40,7 +40,7 @@ describe("index", () => {
         // eslint-disable-next-line @typescript-eslint/no-empty-function -- mock
         using setFailedSpy = vi.spyOn(core, "setFailed").mockImplementation((_s: Error | string) => {});
 
-        await vi.importActual("@/index");
+        await vi.importActual("@/index.ts");
 
         expect(setFailedSpy).toHaveBeenCalledWith(
             "It looks like you're trying to write a test, would you like some assistance? [YES / NO]",
